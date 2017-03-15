a679e8904d0fbaa6724db72646f271325ae1f1d2
LinhNguyen31 <ntrilinh@gmail.com>
2017-03-15 08:07:52 +0700

    Add files via upload

---
+++ 2017_03_15_005817_create_comments_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    private $_table = NULL;
+    private $fileds = NULL;
+
+    public function __construct() {
+        $this->_table = 'comments';
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('comments');
+        Schema::create('comments', function(Blueprint $table)
+        {
+            $table->increments('comment_id');
+            $table->TEXT ('comment_content');
+            $table->integer('comment_id_parrent');
+            $table->integer('user_id');
+            $table->timestamp('comment_date')->nullable();
+            $table->integer('comment_status');
+            $table->integer('post_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
